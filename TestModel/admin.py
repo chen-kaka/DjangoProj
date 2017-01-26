79b38336c4db01f3e23cbd3affeb47387f87dc17
kakachan <chen-kaka@163.com>
2017-01-26 16:33:33 +0800

    admin manipulations

--- TestModel/admin.py
+++ TestModel/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from TestModel.models import Test,Contact,Tag
 
 # Register your models here.
+class TagInline(admin.TabularInline):
+    model = Tag
+
 class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','age', 'email') # list
+    inlines = [TagInline]  # Inline
+    search_fields = ('name',)
+
     fieldsets = (
         ['Main',{
             'fields':('name','email'),
@@ -15,4 +22,4 @@ class ContactAdmin(admin.ModelAdmin):
     )
 
 admin.site.register(Contact, ContactAdmin)
-admin.site.register([Test, Tag])
+admin.site.register([Test])
